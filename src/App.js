f2ee18af9e7cbdf6a2043d7b51eebce41140ca97
Marcos Mathias <marcos-github@memathias.com>
2019-07-25 18:33:15 -0300

    Added History management

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
 import Routes from './routes';
+// Routes can listen the navigation on this history
+import history from './services/history';
 
 function App() {
-  return <Routes />;
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
 
 export default App;
