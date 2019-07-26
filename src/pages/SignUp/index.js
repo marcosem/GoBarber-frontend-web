d50db8e8482c21b817c8d1cab56c8723f90663b3
Marcos Mathias <marcos-github@memathias.com>
2019-07-26 17:41:59 -0300

    Added Unform to Authentication pages

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Full Name" />
-        <input type="email" placeholder="Your e-mail" />
-        <input type="password" placeholder="Password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input name="password" type="password" placeholder="Password" />
         <button type="submit">Create Account</button>
         <Link to="/">Already have an account</Link>
-      </form>
+      </Form>
     </>
   );
 }
