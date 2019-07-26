f8a27c57fe948748e147210b2ab9475813823c7c
Marcos Mathias <marcos-github@memathias.com>
2019-07-26 17:30:35 -0300

    Stylized Authentication pages

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="email" placeholder="Your e-mail" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+        <Link to="/register">Create an account</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input placeholder="Full Name" />
+        <input type="email" placeholder="Your e-mail" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Create Account</button>
+        <Link to="/">Already have an account</Link>
+      </form>
+    </>
+  );
 }
