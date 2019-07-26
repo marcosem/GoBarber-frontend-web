66e8207a9f170f0d80cb1d70f23f7415bd52c994
Marcos Mathias <marcos-github@memathias.com>
2019-07-26 20:20:50 -0300

    Added Authentication Loading

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -7,14 +7,47 @@ const INITIAL_STATE = {
 };
 
 export default function auth(state = INITIAL_STATE, action) {
+  return produce(state, draft => {
+    switch (action.type) {
+      case '@auth/SIGN_IN_REQUEST': {
+        draft.loading = true;
+        break;
+      }
+      case '@auth/SIGN_IN_SUCCESS': {
+        draft.token = action.payload.token;
+        draft.signed = true;
+        draft.loading = false;
+        break;
+      }
+      case '@auth/SIGN_FAILURE': {
+        draft.loading = false;
+        break;
+      }
+      default:
+    }
+  });
+
+  /*
   switch (action.type) {
+    case '@auth/SIGN_IN_REQUEST':
+      return produce(state, draft => {
+        draft.loading = true;
+      });
+
     case '@auth/SIGN_IN_SUCCESS':
       return produce(state, draft => {
         draft.token = action.payload.token;
         draft.signed = true;
+        draft.loading = false;
+      });
+
+    case '@auth/SIGN_FAILURE':
+      return produce(state, draft => {
+        draft.loading = false;
       });
 
     default:
       return state;
   }
+  */
 }
