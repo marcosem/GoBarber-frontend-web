5189c3a5b84ea33e7990e7c7c8c7dc6f4854d15b
Marcos Mathias <marcos-github@memathias.com>
2019-07-29 14:19:39 -0300

    Notification Stylization

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { MdNotifications } from 'react-icons/md';
-import { Container, Badge } from './styles';
+import {
+  Container,
+  Badge,
+  NotificationList,
+  Scroll,
+  Notification,
+} from './styles';
 
 export default function Notifications() {
   return (
@@ -8,6 +14,46 @@ export default function Notifications() {
       <Badge hasUnread>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
+
+      <NotificationList>
+        <Scroll>
+          <Notification unread>
+            <p>You have a new notification!</p>
+            <time>2 days ago</time>
+            <button type="button">Set as readed</button>
+          </Notification>
+          <Notification>
+            <p>You have a new notification!</p>
+            <time>2 days ago</time>
+            <button type="button">Set as readed</button>
+          </Notification>
+          <Notification>
+            <p>You have a new notification!</p>
+            <time>2 days ago</time>
+            <button type="button">Set as readed</button>
+          </Notification>
+          <Notification>
+            <p>You have a new notification!</p>
+            <time>2 days ago</time>
+            <button type="button">Set as readed</button>
+          </Notification>
+          <Notification>
+            <p>You have a new notification!</p>
+            <time>2 days ago</time>
+            <button type="button">Set as readed</button>
+          </Notification>
+          <Notification>
+            <p>You have a new notification!</p>
+            <time>2 days ago</time>
+            <button type="button">Set as readed</button>
+          </Notification>
+          <Notification>
+            <p>You have a new notification!</p>
+            <time>2 days ago</time>
+            <button type="button">Set as readed</button>
+          </Notification>
+        </Scroll>
+      </NotificationList>
     </Container>
   );
 }
