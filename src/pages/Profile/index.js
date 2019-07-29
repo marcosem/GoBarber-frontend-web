0f3bb105de3c64bf69c7f042921e1e95d38b0610
Marcos Mathias <marcos-github@memathias.com>
2019-07-29 17:04:24 -0300

    Built Profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your e-mail address" />
+
+        <hr />
+
+        <Input
+          name="password"
+          type="oldPassword"
+          placeholder="Your current password"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Your new password"
+        />
+        <Input
+          name="password"
+          type="confirmPassword"
+          placeholder="Confirm your new password"
+        />
+
+        <button type="submit">Update profile</button>
+      </Form>
+
+      <button type="button">Logout</button>
+    </Container>
+  );
 }
