58670c16f97416e584e0b33a7799fe105315c835
Marcos Mathias <marcos-github@memathias.com>
2019-07-29 16:40:35 -0300

    Notification management

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { MdNotifications } from 'react-icons/md';
+import { parseISO, formatDistance } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import api from '~/services/api';
+
 import {
   Container,
   Badge,
@@ -9,49 +14,81 @@ import {
 } from './styles';
 
 export default function Notifications() {
+  const [visible, setVisible] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+
+  const hasUnread = useMemo(
+    () => !!notifications.find(notification => notification.read === false),
+    [notifications]
+  );
+
+  const numberOfUnread = useMemo(
+    () =>
+      notifications.filter(notification => notification.read === false).length,
+    [notifications]
+  );
+
+  useEffect(() => {
+    async function loadNotifications() {
+      const response = await api.get('/notifications');
+
+      const data = response.data.map(notification => ({
+        ...notification,
+        timeDistance: formatDistance(
+          parseISO(notification.createdAt),
+          new Date(),
+          { addSuffix: true, locale: pt }
+        ),
+      }));
+
+      setNotifications(data);
+
+      console.tron.log(numberOfUnread);
+    }
+
+    loadNotifications();
+  }, [numberOfUnread]);
+
+  function handleToggleVisible() {
+    setVisible(!visible);
+  }
+
+  async function handleMarkAsRead(id) {
+    await api.put(`/notifications/${id}`);
+
+    setNotifications(
+      notifications.map(notification =>
+        notification._id === id ? { ...notification, read: true } : notification
+      )
+    );
+  }
+
   return (
     <Container>
-      <Badge hasUnread>
+      <Badge
+        onClick={handleToggleVisible}
+        hasUnread={hasUnread}
+        totalUnread={numberOfUnread}
+      >
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
 
-      <NotificationList>
+      <NotificationList visible={visible}>
         <Scroll>
-          <Notification unread>
-            <p>You have a new notification!</p>
-            <time>2 days ago</time>
-            <button type="button">Set as readed</button>
-          </Notification>
-          <Notification>
-            <p>You have a new notification!</p>
-            <time>2 days ago</time>
-            <button type="button">Set as readed</button>
-          </Notification>
-          <Notification>
-            <p>You have a new notification!</p>
-            <time>2 days ago</time>
-            <button type="button">Set as readed</button>
-          </Notification>
-          <Notification>
-            <p>You have a new notification!</p>
-            <time>2 days ago</time>
-            <button type="button">Set as readed</button>
-          </Notification>
-          <Notification>
-            <p>You have a new notification!</p>
-            <time>2 days ago</time>
-            <button type="button">Set as readed</button>
-          </Notification>
-          <Notification>
-            <p>You have a new notification!</p>
-            <time>2 days ago</time>
-            <button type="button">Set as readed</button>
-          </Notification>
-          <Notification>
-            <p>You have a new notification!</p>
-            <time>2 days ago</time>
-            <button type="button">Set as readed</button>
-          </Notification>
+          {notifications.map(notification => (
+            <Notification key={notification._id} unread={!notification.read}>
+              <p>{notification.content}</p>
+              <time>{notification.timeDistance}</time>
+              {!notification.read && (
+                <button
+                  type="button"
+                  onClick={() => handleMarkAsRead(notification._id)}
+                >
+                  Mark as read
+                </button>
+              )}
+            </Notification>
+          ))}
         </Scroll>
       </NotificationList>
     </Container>
