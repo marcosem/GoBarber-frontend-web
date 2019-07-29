fa9924899300c59a95f7e0fb3180be989239eb5b
Marcos Mathias <marcos-github@memathias.com>
2019-07-29 20:05:18 -0300

    Styled Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>May 31</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Chico</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Opened</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Amanda</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
